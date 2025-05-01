e4ef60297bd7ab50e388af41cf03d2475a9de51c
0x0than <136307533+revothan@users.noreply.github.com>
2025-05-01 23:40:54 +0700

    Update main entry point to use globals.css

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
 import App from './App.tsx'
-import './styles/index.css'
+import './styles/globals.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
