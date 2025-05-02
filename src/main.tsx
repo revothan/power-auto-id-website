4d62cafae27df2e4f185fbd7449eed37520f01e8
0x0than <136307533+revothan@users.noreply.github.com>
2025-05-02 10:19:22 +0700

    Update main.tsx to include ScrollToTop component

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
 import App from './App.tsx'
+import ScrollToTop from './components/layout/ScrollToTop'
 import './styles/globals.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <HelmetProvider>
         <App />
       </HelmetProvider>
